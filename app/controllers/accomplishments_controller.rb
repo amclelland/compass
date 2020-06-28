796e439897a770da79711edcf66ac2b262012e8d
maximforever <flufci@gmail.com>
2020-06-28 16:00:21 -0600

    add accomplishment CRUD

--- app/controllers/accomplishments_controller.rb
+++ app/controllers/accomplishments_controller.rb
@@ -1,4 +1,6 @@
 class AccomplishmentsController < ApplicationController
+  before_action :set_accomplishment, only: [:edit, :update, :destroy]
+
   def index
     @accomplishments = current_user.accomplishments
   end
@@ -7,14 +9,42 @@ class AccomplishmentsController < ApplicationController
     @accomplishment = current_user.accomplishments.build(accomplishment_params)
 
     if @accomplishment.save
+      flash[:notice] = 'Accomplishment saved'
+    else
+      flash[:notice] = 'Something went wrong'
+    end
+
+    redirect_to action: :index
+  end
+
+  def destroy
+    if @accomplishment.destroy
+      flash[:notice] = 'Accomplishment unaccomplished'
+    else
+      flash[:notice] = 'Something went wrong'
+    end
+
+    redirect_to action: :index
+  end
+
+  def edit
+  end
+
+  def update
+    if @accomplishment.update_attributes(accomplishment_params)
+      flash[:notice] = 'Accomplishment updated!'
       redirect_to action: :index
     else
       flash[:notice] = 'Something went wrong'
-      redirect_to action: :index
+      redirect_to action: :edit
     end
   end
 
   private
+
+  def set_accomplishment
+    @accomplishment = Accomplishment.find(params[:id])
+  end
 
   def accomplishment_params
     params.require(:accomplishment).permit(:content)
